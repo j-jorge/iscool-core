165768e699ec3455c46e6aeb47768bd94360a992
Julien Jorge <julien.jorge@stuff-o-matic.com>
2018-05-18 11:18:55 +0200

    Add iscool::files::copy_file and iscool::files::rename_file.

---
+++ modules/core/files/module/include/iscool/files/rename_file.h
@@ -0,0 +1,29 @@
+/*
+  Copyright 2018-present IsCool Entertainment
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+#ifndef ISCOOL_FILES_RENAME_FILE_H
+#define ISCOOL_FILES_RENAME_FILE_H
+
+#include <string>
+
+namespace iscool
+{
+    namespace files
+    {
+        bool rename_file( const std::string& from, const std::string& to );
+    }
+}
+
+#endif
