fcc98344fa6ee9dbbf7c1093ef09294c8bf098cf
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-09-21 19:44:18 +0200

    Check if the vibrator is present before trying to use it.

--- modules/core/system/android/java/src/iscool/system/HapticFeedbackService.java
+++ modules/core/system/android/java/src/iscool/system/HapticFeedbackService.java
@@ -3,25 +3,33 @@ package iscool.system;
 import androidx.annotation.Keep;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Vibrator;
 import android.os.VibrationEffect;
 
 @Keep
 public class HapticFeedbackService
 {
-    static private Vibrator _vibrator;
-    static private VibrationEffect _clickEffect;
+    static private Vibrator _vibrator = null;
+    static private VibrationEffect _clickEffect = null;
 
-    static void init( Context context ) {
+    static void init(Context context) {
         _vibrator =
             (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
-        _clickEffect =
-            VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            _clickEffect =
+                VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
     }
 
     static public void click() {
-        if ((_vibrator == null) || (_clickEffect == null))
+        if (_vibrator == null)
             return;
+
+        if (_clickEffect == null) {
+            _vibrator.vibrate(20);
+            return;
+        }
 
         _vibrator.vibrate(_clickEffect);
     }
