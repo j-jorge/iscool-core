257323da0735e00b9dd0f230bcfe967ef0b9022f
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-12-19 21:59:58 +0100

    Update GoogleTest dependency to 1.15.2.

--- build-scripts/cmake/cmake-modules/google-test.cmake
+++ build-scripts/cmake/cmake-modules/google-test.cmake
@@ -18,7 +18,7 @@ include( DownloadProject )
 download_project(
   PROJ google-test
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v1.13.0
+  GIT_TAG v1.15.2
   UPDATE_DISCONNECTED 1
   )
 
