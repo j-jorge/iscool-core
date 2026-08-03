56d353a3a914d98fb548cac30194280292a665fc
Julien Jorge <julien.jorge@stuff-o-matic.com>
2026-08-03 18:55:16 +0200

    Add iscool::json::member_cast.

--- modules/core/json/module/include/iscool/json/cast.impl.tpp
+++ modules/core/json/module/include/iscool/json/cast.impl.tpp
@@ -1,20 +1,5 @@
-/*
-  Copyright 2018-present IsCool Entertainment
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-#ifndef ISCOOL_JSON_CAST_IMPL_TPP
-#define ISCOOL_JSON_CAST_IMPL_TPP
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
 
 #include <iscool/json/cast.hpp>
 #include <iscool/json/detail/cast_fwd.hpp>
@@ -36,4 +21,13 @@ T iscool::json::cast(const Json::Value& value, const T& fallback)
   return cast<T>(value);
 }
 
-#endif
+template <typename T>
+T iscool::json::member_cast(const Json::Value& value, std::string_view key)
+{
+  if (value.type() != Json::objectValue)
+    throw bad_cast(value, "object");
+
+  const Json::Value* const m = value.find(key.begin(), key.end());
+
+  return detail::cast<T>::value(*m);
+}
--- modules/core/json/module/include/iscool/json/implement_cast.hpp
+++ modules/core/json/module/include/iscool/json/implement_cast.hpp
@@ -1,29 +1,10 @@
-/*
-  Copyright 2018-present IsCool Entertainment
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-#ifndef ISCOOL_JSON_DETAIL_IMPLEMENT_JSON_CAST_H
-#define ISCOOL_JSON_DETAIL_IMPLEMENT_JSON_CAST_H
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
 
 #define IMPLEMENT_JSON_CAST(T)                                                \
-  namespace iscool                                                            \
+  namespace iscool::json                                                      \
   {                                                                           \
-    namespace json                                                            \
-    {                                                                         \
-      template T cast<T>(const Json::Value&);                                 \
-      template T cast<T>(const Json::Value&, const T&);                       \
-    }                                                                         \
-  }
-
-#endif
+    template T cast<T>(const Json::Value&);                                   \
+    template T cast<T>(const Json::Value&, const T&);                         \
+    template T member_cast<T>(const Json::Value&, std::string_view);          \
+  }\
