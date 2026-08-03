114da5c3a4d3aa7bd6fab82bcff8c52b5d0922bd
Julien Jorge <julien.jorge@stuff-o-matic.com>
2026-08-03 18:54:23 +0200

    Review iscool::json::is_member.
    No copy of the value nor from its members, and the key is passed as a
    std::string_view.

--- modules/core/json/module/src/iscool/json/is_member.cpp
+++ modules/core/json/module/src/iscool/json/is_member.cpp
@@ -1,28 +1,13 @@
-/*
-  Copyright 2018-present IsCool Entertainment
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+// SPDX-License-Identifier: Apache-2.0
 #include <iscool/json/is_member.hpp>
 
-#include <algorithm>
+#include <json/value.h>
 
-bool iscool::json::is_member(const std::string& key, Json::Value json_value)
+bool iscool::json::is_member(std::string_view key,
+                             const Json::Value& json_value)
 {
   if (json_value.type() != Json::objectValue)
     return false;
 
-  const Json::Value::Members members(json_value.getMemberNames());
-
-  return std::find(members.begin(), members.end(), key) != members.end();
+  return json_value.find(key.begin(), key.end()) != nullptr;
 }
